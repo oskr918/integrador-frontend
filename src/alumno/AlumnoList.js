2c65646dcfad9d19850141528b6cc24c28d554d6
Aleli7 <queenveryhappy.mode@gmail.com>
2023-03-06 20:39:50 -0300

    Update páginas restringidas

---
+++ src/alumno/AlumnoList.js
@@ -0,0 +1,210 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+class AlumnoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alumnoAEliminar:{},
+      modalConfirmarEliminacion:false,
+      alumnos : []
+    };
+    this.onDelete = this.onDelete.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+  }
+
+  handleClose(){
+    this.setState({
+      modalConfirmarEliminacion: false
+    });
+  }
+  handleOpen(alumno){
+    this.setState({
+      alumnoAEliminar: alumno,
+      modalConfirmarEliminacion: true
+    });
+  }
+
+  onDelete() {
+    let request = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json'
+      }
+    };
+
+    fetch(`http://localhost:5000/api/alumno/${this.state.alumnoAEliminar.id}`, request)
+      .then(res => {
+        return res.json().then(body => {
+          return {
+            status: res.status,
+            ok: res.ok,
+            headers: res.headers,
+            body: body
+          };
+        });
+      })
+      .then(result => {
+        if (result.ok) {
+          toast.success(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          this.componentDidMount();
+        } else {
+          toast.error(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      });
+  }
+
+  componentDidMount() {
+    let request = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json',
+        "authorization":sessionStorage.getItem('token')
+      }
+    }; 
+    fetch("http://localhost:5000/api/alumno",request)
+    .then(res => {
+      return res.json().then(body => {
+        return {
+          status: res.status,
+          ok: res.ok,
+          headers: res.headers,
+          body: body
+        };
+      });
+    })
+      .then(result => {
+        if (result.ok) {
+          this.setState({
+            modalConfirmarEliminacion: false,
+            alumnos : result.body
+          });
+        } else {
+          toast.error(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+        
+      },
+    
+        (error) => {
+          console.log(error);
+          this.setState({ 
+            error,
+            alumnos : [],
+            modalConfirmarEliminacion: false
+          });
+        }
+      )
+  }
+  debugger
+  render() {
+    let rowsTable = this.state.alumnos.map((alumno, index) => {
+      return (
+        <>
+        <tr key={index}>
+        <td>{alumno.id}</td>
+        <td>{alumno.nombre}</td>
+        <td>{alumno.apellido}</td>
+        <td>{alumno.dni}</td>
+        <th>
+            <Link to={`/alumno/gest/${alumno.id}`}>
+            <Button variant="primary" type="submit">
+            <i class="fa-solid fa-user-gear"></i>
+              </Button>
+            </Link>
+          </th>
+          <th>
+              <Button variant="danger" type="submit"  onClick={() => this.handleOpen(alumno)}>
+                <i class="fa-solid fa-user-xmark"></i>
+              </Button>
+          </th>
+        </tr>
+        </>
+      )
+    });
+
+    return (
+      <>
+          <div class="container">
+          <div class="card">
+            <img src="../img/ListarAlumno.png" class="card-img" alt="img" />
+            <div class="card-img-overlay">
+            <Link to={`/alumno/gest`}>
+                <Button type="submit" id="add">
+                  Agregar alumno/na
+                </Button>
+              </Link>
+              <table className="table table-striped" id="ListaAl">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>DNI</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {rowsTable}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <Modal
+          show={this.state.modalConfirmarEliminacion}
+          onHide={this.handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton className="dark-content">
+            <Modal.Title>Confirmar eliminación</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            ¿Está seguro que desea eliminar al alumno/a: <strong>{this.state.alumnoAEliminar.apellido} {this.state.alumnoAEliminar.nombre}?</strong>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" onClick={this.handleClose}>
+              Cancelar
+            </Button>
+            <Button variant="danger" onClick={this.onDelete}>Eliminar</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+
+    );
+  }
+}
+
+export default AlumnoList;  
---
+++ src/curso/CursoGest.js
@@ -0,0 +1,187 @@
+import React from "react";
+import {toast } from 'react-toastify';
+import { useNavigate, useParams } from "react-router-dom";
+
+class InternalCursoGest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      nombre: '',
+      descripcion: '',
+      anio: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.props.params.id) {
+      let request = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          "Accept": 'application/json'
+        }
+      };
+
+      fetch(`http://localhost:5000/api/curso/${this.props.params.id}`, request)
+        .then(res => {
+          return res.json().then(body => {
+            return {
+              status: res.status,
+              ok: res.ok,
+              headers: res.headers,
+              body: body
+            };
+          });
+        })
+        .then(result => {
+          if (result.ok) {
+            this.setState({
+              id: result.body.id,
+              nombre: result.body.nombre,
+              apellido: result.body.descripcion,
+              dni: result.body.anio,
+            });
+          } else {
+            toast.error(result.body.message, {
+              position: "bottom-right",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          }
+        })
+    } else {
+      this.setState({
+        id: '',
+        nombre: '',
+        descripcion: '',
+        anio: ''
+      });
+    }
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let data = {
+      id: this.state.id,
+      nombre: this.state.nombre,
+      descripcion: this.state.descripcion,
+      anio: this.state.anio
+    };
+
+    let request = {
+      method: this.props.params.id ? 'PUT' : 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json'
+      }
+    };
+
+    const url = this.props.params.id?`http://localhost:5000/api/curso/${this.props.params.id}`:"http://localhost:5000/api/curso";
+
+    fetch(url, request)
+      .then(res => {
+        return res.json().then(body => {
+          return {
+            status: res.status,
+            ok: res.ok,
+            headers: res.headers,
+            body: body
+          };
+        });
+      })
+      .then(result => {
+        if (result.ok) {
+          toast.success(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          this.props.navigate("/curso/list");
+        } else {
+          toast.error(result.body.message, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  render() {
+    return (
+      <div class="container">
+      <div class="abs-center">
+      <div class="card">
+          <img src="./../img/Gestion.png" class="card-img" alt="img" />
+          <div class="card-img-overlay">
+          <h2 id="hh">{this.props.params.dni ? "Modificando " + this.state.nombre : "Creando curso..."}</h2>
+          </div>
+          <div class="card-img-overlay" id="gest">
+      <div className="row">
+        <div className="col">
+          <form onSubmit={this.handleSubmit} method="POST">
+            <div className="mb-3">
+              <label htmlFor="dni" className="form-label">ID:</label>
+              <input type="text" className="form-control" id="id" name="id" value={this.state.id} onChange={this.handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="nombre" className="form-label">Nombre:</label>
+              <input type="text" className="form-control" id="nombre" name="nombre" value={this.state.nombre} onChange={this.handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="descripcion" className="form-label">Descripción:</label>
+              <input type="text" className="form-control" id="descripcion" name="descripcion" value={this.state.descripcion} onChange={this.handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="anio" className="form-label">Año:</label>
+              <input type="text" className="form-control" id="anio" name="anio" value={this.state.anio} onChange={this.handleChange} />
+            </div>
+            <button type="submit">
+                Guardar
+            </button> 
+          </form>
+        </div>
+      </div>
+      </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+
+export function CursoGest(props) {
+  const navigate = useNavigate();
+  const params = useParams(); 
+  return <InternalCursoGest navigate={navigate} params={params} />
+}
+
+export default CursoGest;
